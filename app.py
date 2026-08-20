447b710a27d6857931e28ce8da1c846b25eca041
Menna Khashan <mnt11122006@gmail.com>
2026-08-20 09:34:32 +0200

    Update Supabase key in app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # --- إعدادات الاتصال بـ Supabase ---
 SUPABASE_URL = "https://jghhfdpidostankjghvr.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaGhmZHBpZG9zdGFua2pnaHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxOTIwNjMsImV4cCI6MjEwMjc2ODA2M30.7hR4OYwh6EHOaCOLubl8jtl6TbXb7EKw9Dr2OArtFW4"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaGhmZHBpZG9zdGFua2pghvrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxOTIwNjMsImV4cCI6MjEwMjc2ODA2M30.7hR4OYwh6EHOaCOLubl8jtl6TbXb7EKw9Dr2OArtFW4"
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # --- إعدادات الحماية لوحة التحكم ---
